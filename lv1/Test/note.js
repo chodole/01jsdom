b0ef852b4912e0915dedb27989d98c1b06b65ae3
chodole <64193990+chodole@users.noreply.github.com>
2024-04-11 17:15:32 +0900

    commit

---
+++ lv1/Test/note.js
@@ -0,0 +1,42 @@
+class Note{
+    sero = [];
+    garo = 0;
+
+    speed = 0;
+    long = 0;
+
+    constructor(sero,garo,speed){
+        this.sero = sero;
+        this.garo = garo;
+        this.speed = speed;
+    }
+}
+
+class NoteDao{
+    noteList = [];
+
+    setNote(){
+        for(let i=0;i<10;i++){
+            let sero = [];
+            let garo = Math.floor(Math.random() * 4);
+            let speed = Math.floor(Math.random() * 10)*100;
+            let long = Math.floor(Math.random()*3)+1;
+
+            console.log("garo = "+garo+" long ="+long);
+
+            if(i> 0){
+                if(this.noteList[i-1].sero.length>=2 && this.noteList[i-1].garo == garo){
+                    i--;
+                }
+            }
+
+            for(let i=0;i<long;i++){
+                sero.push(0);
+            }
+            let note = new Note(sero,garo,speed);
+
+            this.noteList.push(note);
+        }
+    }
+
+}
